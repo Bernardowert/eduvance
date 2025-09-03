f51aa5d22348484351cb101913e2fcfb69722739
devBernardo <bernardowerty@outlook.com>
2025-09-03 13:45:34 -0300

    criação do componente containerGRID

---
+++ src/components/containerGRID/index.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from "react"
+import { twMerge } from "tailwind-merge";
+
+interface ContainerProps{
+    children:ReactNode;
+    className?:string;
+}
+
+
+export function ContainerGRID({children,className}:ContainerProps){
+    const defaultClass = "w-full max-w-containerGRID mx-auto px-4";
+    const combinedClass = twMerge(defaultClass,className);
+    return(
+        <div className={combinedClass}>
+          {children}
+        </div>
+    )
+}
