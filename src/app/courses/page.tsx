851e737ff599fd1f142982bac18fd1b1384fcad8
devBernardo <bernardowerty@outlook.com>
2025-09-19 13:24:40 -0300

    criaçao inicial da HeadCourses

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -1,6 +1,8 @@
 import { CardSection } from "@/components/cardSection";
 
 export default function Courses(){
+
+    
     return(
         <>
                   <CardSection description="Browse expertly curated courses across multiple categories—designed to fit your goals, pace, and passion." classNameTitle="max-w-full" classNameDescription="max-w-cardSection mx-auto">
---
+++ src/components/sections/courses/headCourses/index.tsx
@@ -0,0 +1,14 @@
+import { ContainerGRID } from "@/components/containerGRID";
+
+export function SectionHeadCourses(){
+    return(
+        <section>
+            <ContainerGRID>
+                   <div>
+                        <h3>New Courses</h3>
+                        
+                   </div>
+            </ContainerGRID>
+        </section>
+    )
+}
